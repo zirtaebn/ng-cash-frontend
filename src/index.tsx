f9007cf3813168bdd60e99c264d8f876d4294dfd
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-11-19 12:18:44 -0300

    add global style to the index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
+// components 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// utilities
+import { AuthProvider } from './contexts/Auth/AuthProvider';
+
+// styles
+import { GlobalStyle } from './index.styles';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
+
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <BrowserRouter>
+        <GlobalStyle />
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
